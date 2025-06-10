900de1fb78a8e1a0c525a05b56b648c4d7aa773f
Paulo Catto <paulohcatto@hotmail.com>
2025-06-09 23:42:12 -0300

    feat(vehicles): create reusable card components for trucks and cars

--- src/app/components/car/car-detail/car-detail.component.ts
+++ src/app/components/car/car-detail/car-detail.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Car } from '../../../core/services/models/vehicle.model';
+import { VehicleDataService } from '../../../core/services/vehicle-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-car-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './car-detail.component.html',
-  styleUrl: './car-detail.component.scss'
+  styleUrl: './car-detail.component.scss',
 })
 export class CarDetailComponent {
+  private vehicleService = inject(VehicleDataService);
+  cars: Car[] = [];
 
+  ngOnInit(): void {
+    this.vehicleService.getCars().subscribe({
+      next: (data) => {
+        this.cars = data ?? [];
+      },
+      error: (error) => {
+        this.cars = [];
+        console.log(error);
+      },
+    });
+  }
 }
--- src/app/components/trucks/truck-detail/truck-detail.component.ts
+++ src/app/components/trucks/truck-detail/truck-detail.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Truck } from '../../../core/services/models/vehicle.model';
+import { VehicleDataService } from '../../../core/services/vehicle-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-truck-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './truck-detail.component.html',
-  styleUrl: './truck-detail.component.scss'
+  styleUrl: './truck-detail.component.scss',
 })
 export class TruckDetailComponent {
+  private vehicleService = inject(VehicleDataService);
+  trucks: Truck[] = [];
 
+  ngOnInit(): void {
+    this.vehicleService.getTrucks().subscribe({
+      next: (data) => {
+        this.trucks = data ?? [];
+      },
+      error: (error) => {
+        console.log(error);
+        this.trucks = [];
+      },
+    });
+  }
 }
