de8747a99678ccef50bb159c5e41983248696c84
Paulo Catto <paulohcatto@hotmail.com>
2025-06-14 16:47:38 -0300

    feat: create layout for Bus component

--- src/app/components/bus/bus-detail/bus-detail.component.ts
+++ src/app/components/bus/bus-detail/bus-detail.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Bus } from '../../../core/services/models/vehicle.model';
+import { VehicleDataService } from '../../../core/services/vehicle-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-bus-detail',
-    standalone: true,
-    imports: [],
-    templateUrl: './bus-detail.component.html',
-    styleUrl: './bus-detail.component.scss'
+  selector: 'app-bus-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './bus-detail.component.html',
+  styleUrl: './bus-detail.component.scss',
 })
 export class BusDetailComponent {
+  private vehicleService = inject(VehicleDataService);
+  buses: Bus[] = [];
 
+  ngOnInit(): void {
+    this.vehicleService.getBuses().subscribe({
+      next: (data) => {
+        this.buses = data ?? [];
+      },
+      error: (error) => {
+        this.buses = [];
+        console.error(error);
+      },
+    });
+  }
 }
