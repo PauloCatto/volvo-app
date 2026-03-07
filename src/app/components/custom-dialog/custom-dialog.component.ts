d824f34567f6e9cf8a4032f8ae5682acd12eced9
Paulo Catto <paulohcatto@hotmail.com>
2026-03-07 13:20:51 -0300

    feat: Implement Google Maps component to display store locations and a custom dialog for user interactions.

--- src/app/components/custom-dialog/custom-dialog.component.ts
+++ src/app/components/custom-dialog/custom-dialog.component.ts
@@ -1,16 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-custom-dialog',
-  imports: [MatIconModule],
+  standalone: true,
+  imports: [MatIconModule, MatDialogModule, MatButtonModule, CommonModule],
   templateUrl: './custom-dialog.component.html',
   styleUrls: ['./custom-dialog.component.scss'],
 })
 export class CustomDialogComponent {
   dialogRef = inject(MatDialogRef<CustomDialogComponent>);
-  data = inject<{ title?: string; message: string }>(MAT_DIALOG_DATA);
+  public data = inject<{ title?: string; message: string; subTitle?: string }>(MAT_DIALOG_DATA);
 
   close(): void {
     this.dialogRef.close();
